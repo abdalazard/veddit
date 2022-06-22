85fcbe3686f1d13692c3192f4cd48272ef4dcae4
ABDALAZARD <vinicius.abdala@audeedu.com.br>
2022-06-22 15:32:09 -0300

    Atualiza

---
+++ config/veddit.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE Veddit;
+USE Veddit;
+
+CREATE TABLE Users (
+
+id int primary key auto_increment,
+name varchar(15) unique,
+email varchar(50) unique,
+password varchar(32),
+profile enum("adm", "user")
+);
+
+insert into Users values(null,"ADM","abdalazard@gmail.com",md5("123"),"adm");
--- login/verifyUser.php
+++ login/verifyUser.php
@@ -4,26 +4,26 @@ include "../config/connection.php";
 $email = $_POST['email'];
 $password = MD5($_POST['password']);
 
-$sql = "SELECT * FROM Users WHERE email like $email AND password like $password";
 
-$result = mysqli_query($con, $sql);
+$sql = "SELECT * FROM users WHERE email LIKE '".$email."' AND password LIKE '".$password."'";
+$result = mysqli_query($conn, $sql);
 $total = mysqli_num_rows($result);
 
-if($total > 0) {
-    $dados = mysqli_fetch_array($result);
-    if(!strcmp($password, $dados['password'] )) {
-        $_SESSION['idUser'] = $dados['id'];
-        $_SESSION['email'] = $dados['email'];
-        $_SESSION['password'] = $dados['password'];
+if($total == 0) {
+    $msg = "Login/Senha inválido(s)";
+    header("location:../index.php?msg=".$msg);
+    ?><script>alert( "Senha invalida!")</script>
+    <?php   
+}
 
-        header("location:/timeline/timeline.php");
-    }else{
-        ?><script>alert( "Senha invalida!")</script>
-        <?php
-            header("location: ../index.php");
-        }
-}else{    
-        $msg = "Login/Senha inválido(s)";
-        header("location:../index.php?msg=".$msg); //redirecionamento em PHP
+$dados = mysqli_fetch_array($result);
+if(!strcmp($password, $dados['password'] )) {
+    $_SESSION['idUser'] = $dados['id'];
+    $_SESSION['name'] = $dados['name'];
+    $_SESSION['email'] = $dados['email'];
+    $_SESSION['password'] = $dados['password'];
+    $_SESSION['profile'] = $dados['profile'];
+
+    header("location:/timeline/timeline.php");
 }
 ?>
