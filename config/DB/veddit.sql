3a349fc8e33e8ce1c35b157d38fe7e7fa9d1b7cf
ABDALAZARD <ABDALAZARD@gmail.com>
2022-06-28 01:48:27 -0300

    Atualiza CSS

--- config/DB/veddit.sql
+++ config/DB/veddit.sql
@@ -11,3 +11,17 @@ profile enum("1", "2")
 );
 
 insert into Users values(null,"ADM","abdalazard@gmail.com",md5("123"),"1");
+
+CREATE TABLE Posts (
+
+id int primary key auto_increment,
+user_id int,
+content varchar(255),
+likes INT(3),
+dislike INT(3),
+tags int(2)
+);
+
+
+INSERT INTO Posts VALUES (NULL, 1, "Teste de post", 2, 1);
+
--- index.php
+++ index.php
@@ -14,65 +14,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="shortcut icon" href="images/veddit-logo.png" >
+    <link href="style/style.css" rel="stylesheet">
     <title>Inicio</title>
-    <style>
-        #logo {
-            width: 70px;
-            height: 80px;
-        }
-        #sem-sublinhado {
-            text-decoration: none;
-        }
-        h1 {
-            text-align:center;
-        }       
-        #panel {
-            border-radius: 10px;
-            display: block;
-            margin: 0px auto;
-            width: fit-content;
-
-        }
-        #logo-login-panel {
-            width: 100px;
-            height: 110px;
-            display: block;
-            margin: 0px auto;
-        }
-        #title {
-            color: coral;
-        }        
-        #button {
-            background-color:coral;
-            display:block;
-            margin-top: 10px;
-            color: white;
-            display: block;
-            margin: 0px auto;
-        }
-        #button-group {
-            padding: 30px 15px 15px 30px;
-            background-color: white;
-            border-radius: 10px;
-            display: block;
-            margin: 0px auto;
-            width: fit-content;
-        }
-        #sem-sublinhado {
-            text-decoration: none;
-        }
-        p {
-            text-align:center;
-            color:red;
-        }
-        footer {
-            text-align:center;
-            color:coral;
-            font-size: small;
-        }
-       
-
-    </style>
 </head>
 <body>
     <nav class="navbar bg-light">
