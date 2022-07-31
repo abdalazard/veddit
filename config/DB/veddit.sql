868e254c1a18493c53c72562bc5acb04d1a34159
ABDALAZARD <ABDALAZARD@gmail.com>
2022-07-31 01:09:59 -0300

    Correção de DB

--- config/DB/veddit.sql
+++ config/DB/veddit.sql
@@ -7,10 +7,10 @@ CREATE TABLE Users (
     name varchar(15) unique,
     email varchar(50) unique,
     password varchar(32),
-    profile enum("1", "2"),
+    profile enum("1", "2")
 );
 
-insert into Users values(null,"ADM","abdalazard@gmail.com",md5("123"),"1");
+insert into Users values(null,"ADM","abdalazard@gmail.com",md5("Abdalazard.10"),"1");
 
 CREATE TABLE Groups (
     id int primary key auto_increment,
@@ -19,22 +19,15 @@ CREATE TABLE Groups (
     FOREIGN KEY (creator_id) REFERENCES Users(id)
 );
 
-INSERT INTO Groups VALUES(null, Desabafo, 1);
-
 CREATE TABLE Topics (
     id int primary key auto_increment,
     user_id int,
     group_id int,
     title varchar(255) NOT NULL,
     content varchar(255) NOT NULL,
-    likes INT(3),
-    dislike INT(3),
-    tagName varchar(20),
     FOREIGN KEY (group_id) REFERENCES Groups(id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
-
-INSERT INTO Topics VALUES ( null, 1, 1, "Titulo teste", "blablalbalba", 1, 2, "teste");
 
 CREATE TABLE Comments (
     id int primary key auto_increment,
@@ -43,7 +36,4 @@ CREATE TABLE Comments (
     topic_id int,
     FOREIGN KEY (topic_id) REFERENCES Topics(id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
-
 );
-
-INSERT INTO Comments VALUES ( null, "teste blabla", 1, 1);
