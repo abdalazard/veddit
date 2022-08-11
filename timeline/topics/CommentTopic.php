557bd75d77116b5560702c27fb3f21352223bb09
ABDALAZARD <ABDALAZARD@gmail.com>
2022-08-11 02:08:15 -0300

    att

--- timeline/topics/CommentTopic.php
+++ timeline/topics/CommentTopic.php
@@ -1,18 +1,18 @@
 <?php
 include '../../config/Autentication.php';
-include "../config/DB_Connection.php";
+include '../../config/DB_Connection.php';
 
 $postId = $_POST["postId"];
 $comment = $_POST["comment"];
-$idUser = $_SESSION['idUser'];
+$userId = $_SESSION['idUser'];
 
-$sql = "INSERT INTO Comments Values(NULL, '".$comment."', '".$idUser."', '".$postId."')";
+$sql = "INSERT INTO Comments Values(NULL, '".$comment."', '".$userId."', '".$postId."')";
 
 $result = mysqli_query($conn,$sql);
 
 if(! $result) {
     $msg = "Erro ao comentar";
-    header("location:open_topic.php?idTopic=".$postId."&msg=".$msg."");
+    header("location:./open_topic.php?idTopic=".$postId."&msg=".$msg."");
 }
 
-header("location:open_topic.php?idTopic=".$postId."");
+header("location:./open_topic.php?idTopic=".$postId."");
