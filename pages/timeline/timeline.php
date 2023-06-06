304e4d9c9b917914bc9929b6ed8e4eff8c9d4555
ABDALAZARD <abdalazard@gmail.com>
2023-06-06 16:56:25 -0300

    at

--- pages/timeline/timeline.php
+++ pages/timeline/timeline.php
@@ -30,7 +30,8 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="shortcut icon" href="<?php echo $img_url . '/logo.svg' ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link href="style/style.css" rel="stylesheet">
@@ -42,17 +43,20 @@ try {
         <div class="container-fluid">
             <div class="row">
                 <a href="<?php echo $home_url . 'index.php' ?>" id="sem-sublinhado">
-                    <span id="title" class="navbar-brand"><img src="<?php echo $img_url . '/logo.svg' ?>" alt="VEDDIT" id="logo"></span>
+                    <span id="title" class="navbar-brand"><img src="<?php echo $img_url . '/logo.svg' ?>" alt="VEDDIT"
+                            id="logo"></span>
                 </a>
             </div>
             <div class="dropdown flex-end">
-                <a class="btn dropdown-toggle" href="#" role="button" id="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="btn dropdown-toggle" href="#" role="button" id="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
                     Dashboard
                 </a>
                 <ul class="dropdown-menu m-2">
                     <li><a class="dropdown-item" href="#">Perfil</a></li>
                     <li><a class="dropdown-item" href="#">Configurações</a></li>
-                    <li><a type="text" class="dropdown-item" style="color:red;" href="<?php echo $config_url . '/Logout.php' ?>" alt="Sair"><strong>Logout</strong></a></li>
+                    <li><a type="text" class="dropdown-item" style="color:red;"
+                            href="<?php echo $config_url . '/Logout.php' ?>" alt="Sair"><strong>Logout</strong></a></li>
                 </ul>
             </div>
 
@@ -80,9 +84,10 @@ try {
                 if ($num > 0) {
                     while ($themes = mysqli_fetch_array($resultThemes)) {
             ?>
-                        <div class="col-3 text-center">
-                            <a href="themes/themes_list.php?themeId=<?php echo $themes['id'] ?>" type="button" class="btn"><?php echo $themes['theme']; ?></a>
-                        </div>
+            <div class="col-3 text-center">
+                <a href="themes/themes_list.php?themeId=<?php echo $themes['id'] ?>" type="button"
+                    class="btn"><?php echo $themes['theme']; ?></a>
+            </div>
             <?php
                     }
                 }
@@ -91,17 +96,17 @@ try {
         </div>
         <br>
         <?php if (isset($_GET["msg"])) { ?>
-            <div style="text-align:center;">
-                <p><?php echo $_GET['msg'] ?></p>
-            </div>
+        <div style="text-align:center;">
+            <p><?php echo $_GET['msg'] ?></p>
+        </div>
         <?php } ?>
 
         <div id="feed">
             <?php
             if ($result) {
             ?>
-                <form action="topics/open_topic.php" METHOD="GET">
-                    <?php
+            <form action="topics/open_topic.php" METHOD="GET">
+                <?php
                     $num = mysqli_num_rows($result);
                     if ($num <= 0) {
                         echo "<p style='text-align:center;'>Não existem tópicos disponíveis. </p>";
@@ -109,31 +114,34 @@ try {
                     for ($i = 1; $i <= $num; $i++) {
                         $dados = mysqli_fetch_array($result);
                     ?>
-                        <div id="topic" class="p-2 mb-1 bg-light text-dark">
-                            <a href="topics/open_topic.php?idTopic=<?php echo $dados['id'] ?>" id="sem-sublinhado">
-                                <div id="titleTopic">
-                                    <p><?php echo $dados['title'] ?>
-                                        <?php if ($_SESSION['profile'] == 1) { ?>
-                                            <a href="<?php echo $src_url . "/Topics/DeleteTopic.php?idTopic=" . $dados['id'] ?>" class="btn col-1" id="delete_button"><i class="bi bi-trash3"></i></a>
-                                        <?php } ?>
-                                    </p>
-                                </div>
-
-                            </a>
+                <div id="topic" class="p-2 mb-1 bg-light text-dark">
+                    <a href="topics/open_topic.php?idTopic=<?php echo $dados['id'] ?>" id="sem-sublinhado">
+                        <div id="titleTopic">
+                            <p><?php echo $dados['title'] ?>
+                                <?php if ($_SESSION['profile'] == 1) { ?>
+                                <a href="<?php echo $src_url . "/Topics/DeleteTopic.php?idTopic=" . $dados['id'] ?>"
+                                    class="btn col-1" id="delete_button"><i class="bi bi-trash3"></i></a>
+                                <?php } ?>
+                            </p>
                         </div>
-                    <?php
+
+                    </a>
+                </div>
+                <?php
                     }
                     ?>
-                </form>
+            </form>
             <?php
             }
             ?>
         </div>
     </div>
     <footer> &copy Todos os direitos reservados 2022</footer>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+        integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
     </script>
 </body>
 
