b4d19420be3709e981a01c70e7b4b72d7239eddb
ABDALAZARD <ABDALAZARD@github.com>
2023-06-14 20:13:41 -0300

    update

--- config/DB_Connection.php
+++ config/DB_Connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("localhost", "u877039767_adm", "0g?Hc0d1", "u877039767_abdalazard");
+$conn = mysqli_connect("localhost", "u877039767_adm", "8bM^bfj|", "u877039767_abdalazard");
 // $conn = mysqli_connect("localhost", "root", "123", "veddit");
