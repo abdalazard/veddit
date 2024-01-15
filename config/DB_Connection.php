868236fb2a9db0b41f5aa02e16ee60453ecdd000
abdalazard <abdalazard@gmail.com>
2024-01-15 03:05:02 -0300

    corrige localhost

--- config/DB_Connection.php
+++ config/DB_Connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("localhost", "u877039767_adm", "7[ds=frEMn", "u877039767_abdalazard");
-// $conn = mysqli_connect("localhost", "root", "123", "veddit");
+// $conn = mysqli_connect("localhost", "u877039767_adm", "7[ds=frEMn", "u877039767_abdalazard");
+$conn = mysqli_connect("localhost", "root", "123", "veddit");
